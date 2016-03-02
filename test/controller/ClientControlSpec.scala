4db1995010e60a995251070b8805402634077a4e
Toms <toms.lisments@gmail.com>
2016-03-02 17:47:46 +0200

    add: added play 2.4 database connections, it still tries to connect default db for some reason

--- test/controller/ClientControlSpec.scala
+++ test/controller/ClientControlSpec.scala
@@ -15,35 +15,57 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import org.specs2.mutable._
 import play.api.test.WithApplication
+import play.api.db.{Database, Databases}
+import play.api.db.evolutions._
+
 
 /**
   * Created by kuulart on 16.2.3.
   */
 class ClientControlSpec extends Specification {
 
-  abstract class WithDbData extends WithApplication {
-    override def around[T: AsResult](t: => T): Result = super.around {
-      setupData()
-      t
-    }
+//  abstract class WithDbData extends WithApplication {
+//    override def around[T: AsResult](t: => T): Result = super.around {
+//      setupData()
+//      t
+//    }
+//
+//    def setupData() {
+//      DBs.setup('testdb);
+//      def openDatabaseTransaction() {
+//        def db = NamedDB('testdb).toDB
+//      }
+//    }
+//  }
 
-    def setupData() {
-      DBs.setup('testdb);
-      def openDatabaseTransaction() {
-        def db = NamedDB('testdb).toDB
+  def withMyDatabase[T](block: Database => T) = {
+    Databases.withInMemory(
+      name = "testdb",
+      urlOptions = Map(
+        "MODE" -> "MYSQL"
+      ),
+      config = Map(
+        "logStatements" -> true
+      )
+    ) { testdb =>
+
+      Evolutions.withEvolutions(testdb) {
+        block(testdb)
       }
     }
   }
 
 
-  "Client Controller" should {
-    "respond to the index Action" in new WithDbData{
-      val json = new controllers.clientControl().showAll()(FakeRequest())
-      println(Json.parse(contentAsString(json)))
-      status(json) must equalTo(OK)
 
-      //contentType(result) must beSome("application/xml")
-      //contentAsString(result) must contain("products")
+  "Client Controller" should {
+    "respond to the index Action" in withMyDatabase { testdb=>
+      val connection = testdb.getConnection()
+        val json = new controllers.clientControl().showAll()(FakeRequest())
+        println(Json.parse(contentAsString(json)))
+        status(json) must equalTo(OK)
+
+        //contentType(result) must beSome("application/xml")
+        //contentAsString(result) must contain("products")
     }
     "be retrieved by email" in  new WithApplication{
       // your test code
