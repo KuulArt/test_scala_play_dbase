c57c4d8c4b08376cedc9997a7880dff494157a5f
Toms <toms.lisments@gmail.com>
2016-03-09 19:50:25 +0200

    eod: 09.03.16

--- test/models/UserClientSpecs.scala
+++ test/models/UserClientSpecs.scala
@@ -1,8 +1,25 @@
 package models
+import play.api.test._
+import play.api.test.Helpers._
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+
 
 /**
   * Created by kuulart on 16.9.3.
   */
-class UserClientSpecs {
+class UserClientSpecs extends Specification{
 
+  val appWithMemoryDatabase = FakeApplication(additionalConfiguration = inMemoryDatabase("testdb"))
+
+  step("start application")
+
+  "App should " should {
+    "run an application" in new WithApplication(appWithMemoryDatabase) {
+
+    }
+
+  }
 }
